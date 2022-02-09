b2cf1359c460bf5c90097aad2d2203719aefdded
tKTribal <90782562+tKTribal@users.noreply.github.com>
2022-02-09 14:27:59 -0500

    Initial commit

--- Sources/MinLog/MinLog.swift
+++ Sources/MinLog/MinLog.swift
@@ -1,6 +1,94 @@
-public struct MinLog {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
-    }
+public struct MinLog {
+  
+  enum LoggingType: String {
+    case verbose = "VERBOSE"
+    case debug = "DEBUG"
+    case error = "ERROR"
+    case info = "INFORMATION"
+    case warn = "WARNING"
+  }
+  
+  
+  /// Initialiser
+  private init() {}
+  
+  /// Get File Name
+  /// - Parameter filePath: File Path String
+  /// - Returns: File Name
+  private static func getFileName(filePath: String) -> String {
+    return filePath.components(separatedBy: "/").last!
+  }
+  
+  /// Verbose Logging
+  /// - Parameter message: Message String
+  static func v(
+    _ message: String,
+    filePath: String = #file,
+    functionName: String = #function,
+    lineNumber: Int = #line,
+    columnNumber: Int = #column
+  ){
+    let fileName = MinLog.getFileName(filePath: filePath)
+    let loggingType = LoggingType.verbose.rawValue
+    debugPrint("👄👄👄 \(loggingType): \(fileName) - \(functionName) at line \(lineNumber)[\(columnNumber)]: \(message) 👄👄👄")
+  }
+  
+  /// Debug Logging
+  /// - Parameter message: Message String
+  static func d(
+    _ message: String,
+    filePath: String = #file,
+    functionName: String = #function,
+    lineNumber: Int = #line,
+    columnNumber: Int = #column
+  ){
+    let fileName = MinLog.getFileName(filePath: filePath)
+    let loggingType = LoggingType.debug.rawValue
+    debugPrint("🧑‍💻🧑‍💻🧑‍💻 \(loggingType): \(fileName) - \(functionName) at line \(lineNumber)[\(columnNumber)]: \(message) 🧑‍💻🧑‍💻🧑‍💻")
+  }
+  
+  /// Info Logging
+  /// - Parameter message: Message String
+  static func i(
+    _ message: String,
+    filePath: String = #file,
+    functionName: String = #function,
+    lineNumber: Int = #line,
+    columnNumber: Int = #column
+  ){
+    let fileName = MinLog.getFileName(filePath: filePath)
+    let loggingType = LoggingType.info.rawValue
+    debugPrint("📙📙📙 \(loggingType): \(fileName) - \(functionName) at line \(lineNumber)[\(columnNumber)]: \(message) 📙📙📙")
+  }
+  
+  /// Warning Logging
+  /// - Parameter message: Message String
+  static func w(
+    _ message: String,
+    filePath: String = #file,
+    functionName: String = #function,
+    lineNumber: Int = #line,
+    columnNumber: Int = #column
+  ){
+    let fileName = MinLog.getFileName(filePath: filePath)
+    let loggingType = LoggingType.warn.rawValue
+    debugPrint("🔔🔔🔔 \(loggingType): \(fileName) - \(functionName) at line \(lineNumber)[\(columnNumber)]: \(message) 🔔🔔🔔")
+  }
+  
+  /// Error Logging
+  /// - Parameter message: Message String
+  static func e(
+    _ message: String,
+    filePath: String = #file,
+    functionName: String = #function,
+    lineNumber: Int = #line,
+    columnNumber: Int = #column
+  ){
+    let fileName = MinLog.getFileName(filePath: filePath)
+    let loggingType = LoggingType.error.rawValue
+    debugPrint("❌❌❌ \(loggingType): \(fileName) - \(functionName) at line \(lineNumber)[\(columnNumber)]: \(message) ❌❌❌")
+  }
 }
+
